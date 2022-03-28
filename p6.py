55a045f727114c3abc829873a66a2cf3fadeefb4
vikas2340 <78151270+vikas2340@users.noreply.github.com>
2022-03-28 21:45:33 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+msg=pd.read_csv('Term6DataSet.csv',names=['message','label']) #DataSet6.csv Tabular form data
+print('Total instances in the dataset:',msg.shape[0])
+msg['labelnum']=msg.label.map({'pos':1,'neg':0})
+X=msg.message
+Y=msg.labelnum
+
+print('\nThe message and its label of first 5 instances are listed below')
+for x, y in zip(X[:5],Y[:5]):
+    print(x,',',y)
+
+from sklearn.model_selection import train_test_split
+xtrain,xtest,ytrain,ytest=train_test_split(X,Y)
+print('\nDataset is split into Training and Testing samples')
+print('Total training instances :', xtrain.shape[0])
+print('Total testing instances :', xtest.shape[0])
+
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+xtrain_dtm = count_vect.fit_transform(xtrain) 
+xtest_dtm = count_vect.transform(xtest)
+print('\nTotal features extracted using CountVectorizer:',xtrain_dtm.shape[1])
+print('\nFeatures for first 5 training instances are listed below')
+
+df=pd.DataFrame(xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
+print(df[0:5])
+
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(xtrain_dtm,ytrain)
+predicted = clf.predict(xtest_dtm)
+
+print('\nClassstification results of testing samples are given below')
+for doc, p in zip(xtest, predicted):
+    pred = 'pos' if p==1 else 'neg'
+    print('%s -> %s ' % (doc, pred))
+    
+from sklearn import metrics
+print('\nAccuracy metrics')
+print('Accuracy of the classifer is',metrics.accuracy_score(ytest,predicted))
+print('Recall :',metrics.recall_score(ytest,predicted),
+'\nPrecison :',metrics.precision_score(ytest,predicted))
+print('Confusion matrix')
+print(metrics.confusion_matrix(predicted,ytest))
---
+++ p7.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import pandas as pd
+import numpy as np
+
+iris = datasets.load_iris()
+X = pd.DataFrame(iris.data)
+X.columns = ['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width']
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+
+model = KMeans(n_clusters=3)
+model.fit(X)
+
+colormap = np.array(['red', 'lime', 'black'])
+
+plt.subplot(2, 2, 1)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Targets], s=40)
+plt.title('Real Clusters')
+plt.xlabel('Petal Length')
+plt.ylabel('Petal Width')
+
+plt.subplot(2, 2, 2)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[model.labels_], s=40)
+plt.title('K-Means clustering')
+plt.xlabel('Petal length')
+plt.ylabel('Petal width')
+
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+scaler.fit(X)
+xas = scaler.transform(X)
+xs = pd.DataFrame(xas, columns=X.columns)
+from sklearn.mixture import GaussianMixture
+gmm =GaussianMixture(n_components=3)
+gmm.fit(xs)
+gmm_y = gmm.predict(xs)
+plt.subplot(2, 2, 3)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[gmm_y], s=40)
+plt.title('gmm clustering')
+plt.xlabel('petal length')
+plt.ylabel('petal width')
+print('observation the gmm using em algorithm based clusterig matched')
